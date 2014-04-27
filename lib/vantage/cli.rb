89bba884ddecfc2aa77e12be12d9e9f2c5960334
John Whitson <john.whitson@gmail.com>
2014-04-27 13:58:44 -0600

    Added Vantage::CLI module

---
+++ lib/vantage/cli.rb
@@ -0,0 +1,141 @@
+require 'rubygems'
+
+require 'logger'
+require 'optparse'
+
+module Vantage
+
+  module CLI
+
+    LOGGING_LEVELS = {
+        :debug => Logger::DEBUG,
+        :info => Logger::INFO,
+        :warn => Logger::WARN,
+        :error => Logger::ERROR,
+        :fatal => Logger::FATAL
+    }
+
+    class <<self
+
+      attr_accessor :options
+
+    end
+
+    class CommonOptionParser < ::OptionParser
+
+      attr_accessor :options
+
+      #def options=(value)
+      #  #puts "Setting #{self.class.name}[#{self.object_id}] options => (#{value.class.name}[#{value.object_id}]) #{value}"
+      #  @options = value
+      #end
+      #
+      #def options
+      #  #puts "Getting #{self.class.name}[#{self.object_id}] options. #{@options}"
+      #  @options
+      #end
+
+      def initial_arguments(ia = ARGV)
+        @initial_arguments ||= ia.dup
+      end
+
+      def parse_arguments_from_options_file
+
+      end
+
+      def parse_arguments_from_command_line
+
+      end
+
+      def original_command_line_arguments
+        @original_command_line_arguments
+      end
+
+      def remaining_command_line_arguments
+        @remaining_command_line_arguments
+      end
+
+      def parse_common(command_line_arguments = ARGV, options = { })
+        parse_common!(command_line_arguments.dup, options)
+      end
+
+      def parse_common!(command_line_arguments = ARGV, options = { })
+        #puts "Parsing #{self.class.name}[#{self.object_id}] options. #{@options}"
+        @original_command_line_arguments = command_line_arguments.dup
+        parse!(command_line_arguments)
+        @remaining_command_line_arguments = command_line_arguments.dup
+
+        options_file_path = options[:options_file_path]
+
+        # Make sure that options from the command line override those from the options file
+        parse!(original_command_line_arguments.dup) if options_file_path and load(options_file_path)
+
+        check_required_arguments
+      end
+
+      def required_arguments; @required_arguments ||= [ ] end
+      def add_required_argument(*args)  [*args].each { |arg| required_arguments << arg } end
+      alias :add_required_arguments :add_required_argument
+
+      def missing_required_arguments
+        required_arguments.dup.delete_if { |a| options.has_key?(a.is_a?(Hash) ? a.keys.first : a) }
+      end
+
+      def check_required_arguments
+        _missing_arguments = missing_required_arguments
+        unless _missing_arguments.empty?
+          _missing_arguments = _missing_arguments.pop if _missing_arguments.first.is_a?(Array) # This is a work around where _missing_arguments comes back as an array of an array in Ruby 1.8.7
+          abort "Missing Required Arguments: #{ _missing_arguments.map { |v| (v.is_a?(Hash) ? v.values.first : v).to_s.gsub('_', '-')  }.join(', ') }\n\n#{self.to_s}"
+        end
+      end
+
+      def default_options_file_path
+        file_path = File.join(File.expand_path('.'), "#{File.basename($0, '.rb')}_options")
+        return file_path if File.exists?(file_path)
+        return File.expand_path(File.basename($0, '.*'), '~/.options')
+      end
+
+    end # CommonOptionParser
+
+    def self.new_common_option_parser(*args)
+      op = CommonOptionParser.new(*args)
+      op.options = options
+      op
+    end
+
+  end
+
+end
+@cli_class ||= Vantage::CLI
+def cli_class; @cli_class ||= Vantage::CLI end
+
+@options = options ||= { } #HashTap.new
+def options; @options ||= { } end
+def common_option_parser(_options = options)
+  @common_option_parser ||= begin
+    #cli_class.options ||= _options
+    op = cli_class.new_common_option_parser
+    op.options = _options
+    op
+  end
+end
+
+def options_file_path
+  @options_file_path ||= begin
+    base_name = File.basename($0, '.*')
+    _ofp = File.expand_path("#{base_name}_options")
+    _ofp = File.expand_path(base_name, '~/.options') unless File.exists?(_ofp)
+    _ofp
+  end
+end
+
+def add_common_options(option_parser = common_option_parser, _options = (options || { }))
+  _options[:log_to] ||= STDERR
+  _options[:log_level] ||= 0
+  _options[:options_file_path] ||= option_parser.default_options_file_path if option_parser.respond_to?(:default_options_file_path)
+  option_parser.on('--[no-]options-file [FILENAME]', "\tdefault: #{_options[:options_file_path]}" ) { |v| _options[:options_file_path] = v }
+  option_parser.on('--log-to FILENAME', 'Log file location.', "\tdefault: STDERR") { |v| _options[:log_to] = v }
+  option_parser.on('--log-level LEVEL', Vantage::CLI::LOGGING_LEVELS.keys, "Logging level. Available Options: #{Vantage::CLI::LOGGING_LEVELS.keys.join(', ')}",
+                   "\tdefault: #{Vantage::CLI::LOGGING_LEVELS.invert[_options[:log_level]]}") { |v| _options[:log_level] = Vantage::CLI::LOGGING_LEVELS[v] }
+  option_parser.on('-h', '--help', 'Show this message.') { puts option_parser; exit }
+end
